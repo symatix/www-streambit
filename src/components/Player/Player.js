d26a6d2f3e89b04f46f4af164627ad5e6e5a80d4
Dino Kraljeta <pockshocks@gmail.com>
2018-08-28 19:46:19 +0200

    functional player

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Icon from '../Icons/Icon';
 
 class Player extends React.Component {
    constructor(props) {
@@ -6,6 +7,7 @@ class Player extends React.Component {
       this.state = {
          play: false,
          pause: true,
+         muted: false,
          currentTime: 0
       }
       this.audio = new Audio("portfolio/music/radost1.mp3");
@@ -33,6 +35,16 @@ class Player extends React.Component {
       this.pause();
       this.audio.currentTime = 0;
    }
+   
+   mute = () => {
+      this.audio.muted = !this.audio.muted;
+      this.setState({ muted: !this.state.muted })
+   }
+
+   handleCurrentTime = (e) => {
+       console.log(e.target)
+       this.audio.currentTime = e.target.value;
+   }
 
    displayTime = (time) => {
       const seconds = Math.trunc(time);
@@ -45,14 +57,43 @@ class Player extends React.Component {
       return `${m}:${s}`
    }
 
+    playProgress = () => {
+       if (this.audio.duration) {
+           return <input type="range" min="0" max={this.audio.duration} value={this.audio.currentTime} className="slider" id="myRange" onChange={this.handleCurrentTime}/>
+       }
+       return null;
+   }
+
    render() {
       return (
-         <div>
-            <h1>{this.displayTime(this.state.currentTime)}
-               / {this.displayTime(this.audio.duration)}</h1>
-            <button onClick={this.play}>Play</button>
-            <button onClick={this.pause}>Pause</button>
-            <button onClick={this.stop}>Stop</button>
+         <div className="audio-player">
+            <div className="row">
+               <div className="col-sm-3 audio-player-controls">
+                  <div onClick={this.state.play ? this.pause : this.play}>
+                     <Icon 
+                        id={this.state.play ? "pause" : "play-button"}
+                        fill="rgba(255,255,255,0.9)"/>
+                  </div>
+                  <div onClick={this.stop}>
+                     <Icon 
+                        id={"stop"}
+                        onClick={this.stop}
+                        fill="rgba(255,255,255,0.9)"/>
+                  </div>
+                  <div onClick={this.mute}>
+                     <Icon 
+                        id={this.state.muted ? "mute" : "speaker"}
+                        onClick={this.mute}
+                        fill="rgba(255,255,255,0.9)"/>
+                  </div>
+               </div>
+               <div className="col-sm-6 audio-player-progress">
+                  {this.playProgress()}
+               </div>
+               <div className="col-sm-3 audio-player-info">
+                  <span>{this.displayTime(this.state.currentTime)} / {this.audio.duration ? this.displayTime(this.audio.duration) : '0:0'}</span>
+               </div>
+            </div>
          </div>
       );
    }
